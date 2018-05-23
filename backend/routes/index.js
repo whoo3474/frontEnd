7d1228bc3b21da627df5f2723182634bea3797fa
haru <haru@haru-studio.co.kr>
2018-05-23 19:29:56 +0900

    2018-05-17

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,7 @@
 var express = require('express');
-var admin = require('firebase-admin');
 var router = express.Router();
 
-var serviceAccount = require('../haru-studio-bd682-firebase-adminsdk-2k7zs-1601717c2e');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://haru-studio-bd682.firebaseio.com"
-});
-
-/* GET home page. */
 router.get('/notice', function(request, response, next) {
-  // response.render('index', { title: 'Express' });
   admin.firestore().collection('notice').get().then(function(snapshot) {
     var result = [];
     snapshot.forEach(function(item) {
@@ -19,7 +9,6 @@ router.get('/notice', function(request, response, next) {
     })
     response.json(result);
   })
-  // response.json({agent:request.get('user-agent')})
 });
 
 module.exports = router;

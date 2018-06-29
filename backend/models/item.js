acbb15432b04c4d4b8a7b364d3ee87813d8719f3
Haru K <haru@haru-studio.co.kr>
2018-06-29 11:52:55 +0900

    model definition

--- backend/models/item.js
+++ backend/models/item.js
@@ -36,8 +36,56 @@ function itemColor(instance) {
   })
 }
 
+function itemImage(instance) {
+  return instance.define('itemImage', {
+    item_id:{
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'items',
+        key:'id',
+      }
+    },
+    itemColor_id:{
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'itemColors',
+        key:'id',
+      }
+    },
+    url: Sequelize.STRING
+  })
+}
+
+function itemInfo(instance) {
+  return instance.define('itemInfo', {
+    item_id:{
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'items',
+        key:'id',
+      }
+    },
+    url: Sequelize.STRING,
+    text: Sequelize.STRING,
+  })
+}
+
+function itemDetail(instance) {
+  return instance.define('itemInfo', {
+    item_id:{
+      type: Sequelize.INTEGER,
+      references: {
+        model: 'items',
+        key:'id',
+      }
+    },
+    title: Sequelize.STRING,
+    content: Sequelize.STRING,
+  })
+}
 module.exports = {
   item:item,
   category:category,
-  itemColor:itemColor
+  itemColor:itemColor,
+  itemImage:itemImage
 };
